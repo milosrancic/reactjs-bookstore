99798c8f5e5ebf176b85c5f9f0c27ffa33712c5e
Milos <milosrancic94@gmail.com>
2019-04-16 21:14:54 +0200

    /

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,12 +1,25 @@
 import React from "react";
 import "../styles/Cart.css";
 
-const Cart = props => (
-  <div className="col-md-4 ">
-    <p>
-      <small>total:</small> ${props.cart.toFixed(2)}
-    </p>
-  </div>
-);
+class Cart extends React.Component {
+  render() {
+    let id,
+      price,
+      title = undefined;
+    // for (let i of this.props.pickedBook) {
+    //   id = i.id;
+    //   price = i.price;
+    //   title = i.volumeInfo.title;
+    // }
+    console.log(this.props.pickedBook.price);
+    // console.log(id, title, price);
+    return (
+      <div className="col-md-4 ">
+        <small>total:</small> ${this.props.cart.toFixed(2)}
+        {/* ${this.props.cart.toFixed(2)} */}
+      </div>
+    );
+  }
+}
 
 export default Cart;
