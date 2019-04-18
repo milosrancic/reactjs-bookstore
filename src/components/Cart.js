875443c0c548c601a6b99d0e1a05ccfd7686ee3a
Milos <milosrancic94@gmail.com>
2019-04-18 19:31:19 +0200

    changed into functional component, added x next to cart value

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,19 +1,18 @@
 import React from "react";
 import "../styles/Cart.css";
 
-class Cart extends React.Component {
-  render() {
-    const books = this.props.cart;
-    // let id = books.id;
-    // let price = books.price;
-    // console.log(`price ${price} `);
-    // console.log(`id ${id} `);
-    return (
-      <div className="col-md-4 ">
-        <small>total:</small> ${this.props.cart.toFixed(2)}
-      </div>
-    );
-  }
-}
+const Cart = props => (
+  <div className="col-md-4 ">
+    <small>total:</small> ${props.cart.toFixed(2)}
+    <sup>
+      {" "}
+      {props.cart !== 0 ? (
+        <button onClick={props.onClick}> x </button>
+      ) : (
+        ""
+      )}{" "}
+    </sup>
+  </div>
+);
 
 export default Cart;
