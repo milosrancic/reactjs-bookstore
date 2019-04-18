8b2594a1df34c8f42620b6a89c5882f64fb668b5
Milos <milosrancic94@gmail.com>
2019-04-18 19:27:02 +0200

    changed into functional component

--- src/components/BookItem.js
+++ src/components/BookItem.js
@@ -1,79 +1,73 @@
 import React from "react";
 import "../styles/BookItem.css";
 
-class BookItem extends React.Component {
-  render() {
-    const books = this.props.books;
+const BookItem = props => (
+  <div className="row">
+    <ul>
+      {props.books.map(book => (
+        <li key={book.id}>
+          <div className="row row-book">
+            <div className="col-sm text-center">
+              <h4>
+                {book.volumeInfo.title.length > 50
+                  ? `${book.volumeInfo.title.slice(0, 50)}...`
+                  : book.volumeInfo.title}
+              </h4>
+              <p>author: {book.volumeInfo.authors}</p>
+              <p>
+                <small>
+                  {book.volumeInfo.pageCount === undefined
+                    ? ""
+                    : `pages: ${book.volumeInfo.pageCount}`}
+                </small>
+              </p>
 
-    return (
-      <div className="row">
-        <ul>
-          {books.map(book => (
-            <li key={book.id}>
-              <div className="row row-book">
-                <div className="col-sm text-center">
-                  <h4>
-                    {book.volumeInfo.title.length > 50
-                      ? `${book.volumeInfo.title.slice(0, 50)}...`
-                      : book.volumeInfo.title}
-                  </h4>
-                  <p>author: {book.volumeInfo.authors}</p>
-                  <p>
-                    <small>
-                      {book.volumeInfo.pageCount === undefined
-                        ? ""
-                        : `pages: ${book.volumeInfo.pageCount}`}
-                    </small>
-                  </p>
+              <p className="text-center">
+                <span>${book.price}</span>
+                <button
+                  onClick={() => props.onClick(book)}
+                  className="btn btn-sm btn-outline-secondary m-3"
+                >
+                  buy
+                </button>
+              </p>
+            </div>
 
-                  <p className="text-center">
-                    <span>${book.price}</span>
-                    <button
-                      onClick={() => this.props.onClick(book)}
-                      className="btn btn-sm btn-outline-secondary m-3"
-                    >
-                      buy
-                    </button>
-                  </p>
-                </div>
-
-                <div className="col-sm col-img">
-                  <img
-                    className="img-fluid"
-                    alt={book.volumeInfo.title}
-                    src={
-                      book.volumeInfo.imageLinks === undefined
-                        ? ""
-                        : `${book.volumeInfo.imageLinks.thumbnail}`
-                    }
-                  />
-                </div>
-                <div className="col-sm ">
-                  <p>
-                    {book.volumeInfo.description === undefined
-                      ? " "
-                      : book.volumeInfo.description.length > 300
-                      ? `${book.volumeInfo.description.slice(0, 300)}...`
-                      : book.volumeInfo.description.slice(0, 300)}
-                  </p>
-                  <p className="text-center ">
-                    <a
-                      href={`${book.volumeInfo.infoLink}`}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      className="this-edit"
-                    >
-                      more info
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </li>
-          ))}
-        </ul>
-      </div>
-    );
-  }
-}
+            <div className="col-sm col-img">
+              <img
+                className="img-fluid"
+                alt={book.volumeInfo.title}
+                src={
+                  book.volumeInfo.imageLinks === undefined
+                    ? ""
+                    : `${book.volumeInfo.imageLinks.thumbnail}`
+                }
+              />
+            </div>
+            <div className="col-sm ">
+              <p>
+                {book.volumeInfo.description === undefined
+                  ? " "
+                  : book.volumeInfo.description.length > 300
+                  ? `${book.volumeInfo.description.slice(0, 300)}...`
+                  : book.volumeInfo.description.slice(0, 300)}
+              </p>
+              <p className="text-center ">
+                <a
+                  href={`${book.volumeInfo.infoLink}`}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="more-info-link"
+                >
+                  more info
+                </a>
+              </p>
+            </div>
+          </div>
+        </li>
+      ))}
+    </ul>
+  </div>
+);
 
 export default BookItem;
