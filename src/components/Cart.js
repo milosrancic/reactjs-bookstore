8ab5ad79bd19fcaca5e01f830bb99c6c5554e9a7
Milos <milosrancic94@gmail.com>
2019-04-21 17:31:33 +0200

    fixed x button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,14 +4,16 @@ import "../styles/Cart.css";
 const Cart = props => (
   <div className="cart col-md-4">
     <small>total:</small> ${props.cart.toFixed(2)}
-    <sup>
-      {" "}
-      {props.cart !== 0 ? (
-        <button onClick={props.onClick}> x </button>
-      ) : (
-        ""
-      )}{" "}
-    </sup>
+    {/* <sup> */}{" "}
+    {props.cart !== 0 ? (
+      <button className="button cart-btn" type="button" onClick={props.onClick}>
+        {" "}
+        x{" "}
+      </button>
+    ) : (
+      ""
+    )}{" "}
+    {/* </sup> */}
   </div>
 );
 
