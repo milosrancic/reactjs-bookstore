d048c5426958d50cd560faa9f248eac70196d74b
Milos <milosrancic94@gmail.com>
2020-02-05 16:18:18 +0100

    navigation logic finished

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/SearchBar.css";
 
 const SearchBar = props => (
-  <React.Fragment>
-    <div className="col-md-4 ">
-      <h2>books</h2>
-    </div>
+  <nav id="navbar" className="container-fluid mb-4 p-0">
+    <ul className="nav justify-content-center">
+      <li className="nav-item">
+        <Link to={"/"} className="nav-link">
+          <>books</>
+        </Link>
+      </li>
 
-    <div className="col-md-4 ">
-      <form
-        onSubmit={props.onFormSubmit}
-        className="form-inline justify-content-center "
-      >
-        <input
-          value={props.value}
-          onChange={props.onInputChange}
-          className="form-control form-control-sm mr-3 w-75"
-          type="text"
-          placeholder="Search for book"
-          aria-label="Search"
-          required
-        />
-        <button type="submit" className="btn btn-sm btn-outline-success">
-          <i className="fas fa-search" aria-hidden="true" />
-        </button>
-      </form>
-    </div>
-  </React.Fragment>
+      <li className="nav-item">
+        <form
+          onSubmit={props.onFormSubmit}
+          className="form-inline justify-content-center "
+        >
+          <input
+            value={props.value}
+            onChange={props.onInputChange}
+            className="form-control form-control-sm mr-3 w-75"
+            type="text"
+            placeholder="Search for book"
+            aria-label="Search"
+            required
+          />
+          <button type="submit" className="btn btn-sm btn-outline-success">
+            <i className="fas fa-search" aria-hidden="true" />
+          </button>
+        </form>
+      </li>
+
+      <li className="nav-item">
+        <Link to={"/cart"} className="nav-link">
+          <i class="fas fa-shopping-cart"></i>
+        </Link>
+      </li>
+    </ul>
+  </nav>
 );
 
 export default SearchBar;
