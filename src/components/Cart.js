50d17269becb270f5e870ea42ae486f36a464ea4
Milos <milosrancic94@gmail.com>
2020-02-11 17:49:50 +0100

    added "proceed to checkout" button

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -37,10 +37,21 @@ const Cart = props => (
     </ol>
 
     {props.totalCart > 0 && props.totalCart !== 0 ? (
-      <p className="float-right total-parapraph">
-        <small>total: </small>
-        <strong>${props.totalCart.toFixed(2)}</strong>
-      </p>
+      <div className="float-right">
+        <p className="total-parapraph">
+          <small>total: </small>
+          <strong>${props.totalCart}</strong>
+        </p>
+        <p>
+          <button
+            type="button"
+            className="btn btn-outline-success "
+            onClick={() => alert("thanks for buying")}
+          >
+            Proceed to checkout
+          </button>
+        </p>
+      </div>
     ) : (
       <h6 className="mt-5">Your cart is empty! :(</h6>
     )}
