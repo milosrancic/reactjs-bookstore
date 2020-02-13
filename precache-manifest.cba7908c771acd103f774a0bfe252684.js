5f92daf9e0288a713f2c34bdd8c58e6183d261fc
Milos <milosrancic94@gmail.com>
2020-02-13 22:06:41 +0100

    Updates

---
+++ precache-manifest.cba7908c771acd103f774a0bfe252684.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "63a538c5b200f538c18d",
+    "url": "/reactjs-bookstore/static/css/main.9aab1345.chunk.css"
+  },
+  {
+    "revision": "63a538c5b200f538c18d",
+    "url": "/reactjs-bookstore/static/js/main.a5abf123.chunk.js"
+  },
+  {
+    "revision": "e43380e5a30289306442",
+    "url": "/reactjs-bookstore/static/js/runtime~main.fc9acc52.js"
+  },
+  {
+    "revision": "49f5869ba20dd8aac37b",
+    "url": "/reactjs-bookstore/static/css/2.6747beb9.chunk.css"
+  },
+  {
+    "revision": "49f5869ba20dd8aac37b",
+    "url": "/reactjs-bookstore/static/js/2.566ba9e2.chunk.js"
+  },
+  {
+    "revision": "00842ec214c5679c97eab905ff112e52",
+    "url": "/reactjs-bookstore/index.html"
+  }
+];
